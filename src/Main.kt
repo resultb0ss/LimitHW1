3559e6cbc1ad059316633b1afd69af1063f33185
Admin <reteamboss@gmail.com>
2024-09-07 15:00:33 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+
+    var array = intArrayOf(1,2,3,4,5,6,7,8,9)
+    println(States<Int>().getAverage(array))
+}
+
+class States<T>() where T:Number{
+
+    fun getAverage(array: IntArray): Number{
+        return array.average()
+    }
 }
